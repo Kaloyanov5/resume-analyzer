307bfa1d38aba03c7b8fdf993df241af61c410a4
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-03-05 22:15:57 +0200

    Boilerplate code for `GeminiService`

--- src/main/java/spring/project/resume_analyzer/service/GeminiService.java
+++ src/main/java/spring/project/resume_analyzer/service/GeminiService.java
@@ -1,7 +1,20 @@
 package spring.project.resume_analyzer.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class GeminiService {
+    private final String URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
+
+    @Value("${GEMINI_API_KEY}")
+    private String API_KEY;
+
+    private final List<Map<String, Object>> conversationHistory = new ArrayList<>();
+    private final RestTemplate restTemplate = new RestTemplate();
 }
