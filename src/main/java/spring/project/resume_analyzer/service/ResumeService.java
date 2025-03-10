24a09d02a2e1dd87c726d4bead8d4fe41ca62527
Kaloyanov5 <kaloyanovbojidar@gmail.com>
2025-03-10 22:40:43 +0200

    Updated `ResumeService`

--- src/main/java/spring/project/resume_analyzer/service/ResumeService.java
+++ src/main/java/spring/project/resume_analyzer/service/ResumeService.java
@@ -1,10 +1,14 @@
 package spring.project.resume_analyzer.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import spring.project.resume_analyzer.repository.ResumeRepository;
+import spring.project.resume_analyzer.request.AnalyzationRequest;
 import spring.project.resume_analyzer.request.ResumeRequest;
 
 @Service
@@ -12,25 +16,34 @@ public class ResumeService {
 
     @Autowired
     private ResumeRepository resumeRepository;
-
     @Autowired
     private GeminiService geminiService;
-
     @Autowired
     private UtilService utilService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public ResponseEntity<?> analyzeResume(ResumeRequest resumeRequest) {
-        MultipartFile resume = resumeRequest.getResume();
-        if (resume.isEmpty())
-            return ResponseEntity.badRequest().body("Please upload a valid resume file.");
-        String email = resumeRequest.getEmail();
+        try {
+            MultipartFile resume = resumeRequest.getResume();
+            if (resume.isEmpty())
+                return ResponseEntity.badRequest().body("Please upload a valid resume file.");
+            String email = resumeRequest.getEmail();
 
-        String fileContent = utilService.extractTextFromFile(resume);
-        if (fileContent == null)
-            return ResponseEntity.internalServerError().body("Error extracting text from file!");
+            String fileContent = utilService.extractTextFromFile(resume);
+            if (fileContent == null)
+                return ResponseEntity.internalServerError().body("Error extracting text from file!");
 
+            ResponseEntity<?> aiResponse = geminiService.geminiAnalyzeResume(fileContent);
+            if (aiResponse.getStatusCode() != HttpStatus.OK)
+                return ResponseEntity.status(aiResponse.getStatusCode()).body(aiResponse.getBody());
 
+            AnalyzationRequest responseObject = objectMapper.readValue(aiResponse.getBody().toString(), AnalyzationRequest.class);
+            utilService.sendResultsToEmail(email, responseObject);
 
-        return ResponseEntity.ok("");
+            return ResponseEntity.ok("Resume analyzed successfully!");
+        } catch (JsonProcessingException e) {
+            return ResponseEntity.internalServerError().body("Error: " + e.getMessage());
+        }
     }
 }
